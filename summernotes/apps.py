5d6ac62524e27a09308f3b7deb06a308209ea622
fernando <ftenorio_561@hotmail.com>
2020-04-28 17:40:20 -0300

    inclusão models

---
+++ summernotes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SummernotesConfig(AppConfig):
+    name = 'summernotes'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fernando/Python/Django/blog/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
